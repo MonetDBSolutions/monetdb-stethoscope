a8b1eca4714f13b5a660b0b34d98d2e416a42c5b
Panagiotis Koutsourakis <kutsurak@zarniwoop.org>
2020-01-15 10:06:58 +0100

    Refactor parsing
    We are now able to define parsing abstractly.

---
+++ monetdb_profiler_tools/parsing.py
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License, v. 2.0. If a copy of the MPL was not
+# distributed with this file, You can obtain one at
+# https://mozilla.org/MPL/2.0/.
+
+
+import json
+import logging
+from monetdb_profiler_tools.filtering import identity_function
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parser_wrapper(json_str):
+    """JSON parser with exception logging"""
+    try:
+        return json.loads(json_str)
+    except Exception as e:
+        LOGGER.warn("Parsing failed for %s (%s)", json_str, e)
+        return dict()
+
+
+def json_parser():
+    """Returns a callable that can parse JSON strings."""
+    return parser_wrapper
+
+
+def identity_parser():
+    return identity_function
--- monetdb_profiler_tools/stethoscope.py
+++ monetdb_profiler_tools/stethoscope.py
@@ -6,14 +6,15 @@
 """This is the implementation of stethoscope, a tool to interact with MonetDB
 profiler streams."""
 
-import json
 import logging
 import sys
 import click
 import pymonetdb
 from monetdb_profiler_tools.filtering import include_filter, exclude_filter
 from monetdb_profiler_tools.filtering import identity_filter
-from monetdb_profiler_tools.formatting import line_formatter, raw_format
+from monetdb_profiler_tools.formatting import line_formatter, raw_formatter
+from monetdb_profiler_tools.formatting import json_formatter
+from monetdb_profiler_tools.parsing import json_parser, identity_parser
 
 LOGGER = logging.getLogger(__name__)
 
@@ -24,13 +25,13 @@ LOGGER = logging.getLogger(__name__)
               help="A comma separated list of keys. Filter out all other keys.")
 @click.option("--exclude-keys", "-e", "exclude",
               help="A comma separated list of keys to exclude")
-@click.option("--formatter", "-f", "fmt", default="raw")
+@click.option("--formatter", "-f", "fmt", default="json")
 @click.option("--raw", "-r", "raw", is_flag=True)
 @click.option("--output", "-o", "outfile", default="stdout")
 def stethoscope(database, include, exclude, fmt, raw, outfile):
     """A flexible tool to manipulate MonetDB profiler streams"""
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 
     LOGGER.debug("Input arguments")
     LOGGER.debug("  Database: %s", database)
@@ -43,33 +44,44 @@ def stethoscope(database, include, exclude, fmt, raw, outfile):
     cnx = pymonetdb.ProfilerConnection()
     cnx.connect(database, username='monetdb', password='monetdb', heartbeat=0)
 
-    if include:
-        operator = include_filter(include.split(','))
-    elif exclude:
-        operator = exclude_filter(exclude.split(','))
+    if not raw:
+        parse_operator = json_parser()
     else:
-        operator = identity_filter
+        parse_operator = identity_parser()
 
-    if fmt == "raw":
-        formatter = raw_format
-    if fmt == "line":
+    if include:
+        filter_operator = include_filter(include.split(','))
+    elif exclude:
+        filter_operator = exclude_filter(exclude.split(','))
+    else:
+        filter_operator = identity_filter()
+
+    if fmt == "json":
+        formatter = json_formatter
+    elif fmt == "line":
         formatter = line_formatter
+    else:
+        formatter = raw_formatter
 
     if raw:
         if include:
             LOGGER.warning("Ignoring include keys because --raw was specified")
         if exclude:
             LOGGER.warning("Ignoring exclude keys because --raw was specified")
-        if fmt and fmt != "raw":
+        if fmt and fmt != "json":
             LOGGER.warning("Ignoring formatter %s because --raw was specified", fmt)
 
-        formatter = raw_format
-        operator = identity_filter
+        formatter = raw_formatter
 
     out_file = sys.stdout
     if outfile != "stdout":
         out_file = open(outfile, "w")
 
     while True:
-        json_object = operator(json.loads(cnx.read_object()))
-        formatter(json_object, out_file)
+        try:
+            s = cnx.read_object()
+            d = parse_operator(s)
+            json_object = filter_operator(d)
+            formatter(json_object, out_file)
+        except Exception as e:
+            LOGGER.warn("Failed operating on %s (%s)", s, e)
