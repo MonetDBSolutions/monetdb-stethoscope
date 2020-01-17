e3b2a58905af09e1966c49c04e29d0726a26a273
Panagiotis Koutsourakis <kutsurak@zarniwoop.org>
2020-01-17 14:57:33 +0100

    Finalize statement reconstruction

--- monetdb_profiler_tools/transformers.py
+++ monetdb_profiler_tools/transformers.py
@@ -11,37 +11,78 @@ LOGGER = logging.getLogger(__name__)
 
 
 def statement_reconstructor(json_object):
-    stmt = "{}.{}".format(json_object.get("module"),
-                          json_object.get("function"))
+    module = json_object.get("module")
+    function = json_object.get("function")
+
+    program = json_object.get("program")
+    operator = json_object.get("operator")
+
+    if operator is None:
+        stmt = f"{module}.{function}"
+    else:
+        if operator == 'function':
+            stmt = f'{operator} {program}()'
+        else:
+            stmt = f'{operator} {program}'
+
+
+    default_values = {
+        "int": 0,
+        "str": '""'
+    }
 
     args = ""
     rets = ""
+    ret_type = ""
     ret_num = 0
+
     for arg in json_object.get("args", []):
+        var_name = arg.get("var", "undefined")
+        vtype = arg.get("type", "undefined")
         if arg.get("ret") is not None:
             if ret_num > 0:
-                ret_str = ','
-            ret_str = '{}:{}'.format(
-                arg.get("var"),
-                arg.get("type")
-            )
+                rets += ','
+                ret_type += ','
+
+            ret_str = f'{var_name}:{vtype}'
             ret_num += 1
             rets += ret_str
+            ret_type += vtype
         else:
+            value = arg.get("value", None)
+            count = arg.get("count", None)
+            default = default_values.get(vtype, "unknown")
+            const = bool(arg.get("const", False))
+
             if arg.get("arg") > ret_num:
                 args += ','
-            arg_str = "{}=[{}]:{}".format(
-                arg.get("var"),
-                arg.get("count"),
-                arg.get("type")
-            )
+
+            if const:
+                arg_str = f"{value}:{vtype}"
+            elif value is not None:
+                arg_str = f"{var_name}={value}:{vtype}"
+            elif count is None:
+                arg_str = f"{var_name}={default}:{vtype}"
+            else:
+                arg_str = f"{var_name}=[{count}]:{vtype}"
             args += arg_str
 
-    statement = '{} := {}({})'.format(
-        rets,
-        stmt,
-        args
-    )
+    if  operator is not None:
+        statement = f'{stmt}'
+        if operator == 'function':
+            if ret_num == 1:
+                statement += f":{ret_type};"
+            elif ret_num > 1:
+                statement += f":({ret_type});"
+            else:
+                statement += ';'
+    elif ret_num == 0:
+        statement = f'{stmt}({args});'
+    elif ret_num == 1:
+        statement = f'{rets} := {stmt}({args});'
+    else:
+        statement = f'({rets}) := {stmt}({args});'
 
-    json_object['stmt_rec'] = statement
-    return json_object
+    rdict = dict(json_object)
+    rdict['stmt'] = statement
+    return rdict
