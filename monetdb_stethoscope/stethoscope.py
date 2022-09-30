214f3a1734000515b9a57efa6842ee3a8330725c
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2022-09-30 17:03:08 +0300

    Add copyright notice in the source

--- monetdb_stethoscope/stethoscope.py
+++ monetdb_stethoscope/stethoscope.py
@@ -2,6 +2,8 @@
 # Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at
 # https://mozilla.org/MPL/2.0/.
+#
+# Copyright 1997 - July 2008 CWI, August 2008 - 2022 MonetDB B.V.
 
 """This is the implementation of stethoscope, a tool to interact with MonetDB
 profiler streams."""
--- monetdb_stethoscope/utilities.py
+++ monetdb_stethoscope/utilities.py
@@ -2,6 +2,8 @@
 # Public License, v. 2.0. If a copy of the MPL was not
 # distributed with this file, You can obtain one at
 # https://mozilla.org/MPL/2.0/.
+#
+# Copyright 1997 - July 2008 CWI, August 2008 - 2022 MonetDB B.V.
 
 import logging
 
